2252e3f50729e2f1fa6502146f8524b532a869a3
eimonkyaw <eimonkyaw@cloudsource.co.jp>
2015-05-28 11:59:19 +0630

    Initial Commit

---
+++ HousingSNS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HousingSNS
+//
+//  Created by EiMon on 5/28/15.
+//  Copyright (c) 2015 cloudsource. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
