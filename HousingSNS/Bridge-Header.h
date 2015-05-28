5a1c24786fb899cc523d321a0c8f5edfca48b53e
eimonkyaw <eimonkyaw@cloudsource.co.jp>
2015-05-28 13:15:20 +0630

    SNS
    Contain FB login

---
+++ HousingSNS/Bridge-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridge-Header.h
+//  HousingSNS
+//
+//  Created by EiMon on 5/28/15.
+//  Copyright (c) 2015 cloudsource. All rights reserved.
+//
+
+#ifndef HousingSNS_Bridge_Header_h
+#define HousingSNS_Bridge_Header_h
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
